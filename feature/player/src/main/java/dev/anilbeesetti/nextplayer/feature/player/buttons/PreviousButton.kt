b81a4bf9a97457c4a180d6fb612c6eb536d693ec
anonymous-shanks <shanks9628@gmail.com>
2026-09-23 07:36:32 +0530

    Update PreviousButton.kt

--- feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/buttons/PreviousButton.kt
+++ feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/buttons/PreviousButton.kt
@@ -4,30 +4,47 @@ import androidx.annotation.OptIn
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
-import androidx.media3.ui.compose.state.rememberPreviousButtonState
 import dev.anilbeesetti.nextplayer.core.ui.R as coreUiR
 import dev.anilbeesetti.nextplayer.feature.player.LocalControlsVisibilityState
 
 @OptIn(UnstableApi::class)
 @Composable
 internal fun PreviousButton(player: Player, modifier: Modifier = Modifier) {
-    val state = rememberPreviousButtonState(player)
+    var hasPrevious by remember(player) { mutableStateOf(player.hasPreviousMediaItem()) }
+
+    DisposableEffect(player) {
+        val listener = object : Player.Listener {
+            override fun onEvents(player: Player, events: Player.Events) {
+                if (events.containsAny(Player.EVENT_MEDIA_ITEM_TRANSITION, Player.EVENT_TIMELINE_CHANGED)) {
+                    hasPrevious = player.hasPreviousMediaItem()
+                }
+            }
+        }
+        player.addListener(listener)
+        onDispose {
+            player.removeListener(listener)
+        }
+    }
+
     val controlsVisibilityState = LocalControlsVisibilityState.current
 
     PlayerButton(
         modifier = modifier.size(48.dp),
-        isEnabled = state.isEnabled,
+        isEnabled = hasPrevious,
         onClick = {
-            if (player.hasPreviousMediaItem()) {
+            if (hasPrevious) {
                 player.seekToPreviousMediaItem()
-            } else {
-                player.seekTo(0)
             }
             controlsVisibilityState?.showControls()
         },
